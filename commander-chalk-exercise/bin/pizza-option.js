3c61e05e543e961f8e80f811e1cd7d4ec7d6fcd8
windofme1109 <wuzengyan1314@163.com>
2021-11-10 23:38:33 +0800

    commander 中的 on 方法与 name 和 usage 方法的基本使用

--- commander-chalk-exercise/bin/pizza-option.js
+++ commander-chalk-exercise/bin/pizza-option.js
@@ -4,12 +4,22 @@ const {Command} = require('commander');
 
 const program = new Command();
 
+
+program
+    .command('set <type>')
+    .description('set pizza size')
+
+program
+    .option('-d, --debug', 'output extra debugging')
+    .option('-s, --small', 'small pizza size')
+
+
 // program
 //     .option('-d, --debug', 'output extra debugging')
 //     .option('-s, --small', 'small pizza size')
 //     .option('-p, --pizza-type <type>', 'flavour of pizza')
 //     .option('-c, --cheese <type>', 'add the specified type of cheese', 'blue');
-//
+
 //
 // program
 //     .option('--no-sauce', 'Remove sauce')
@@ -20,16 +30,43 @@ const program = new Command();
 //     .option('-t, --type [value]', 'pizza type')
 //     .requiredOption('-c, --cheese <type>', 'pizza must have cheese');
 
+// program
+//     .name('pizza')
+//     .usage('<command> [option]')
+
+// program
+//     .command('get <pizza>')
+//     .description('get a pizza')
+
+// program
+//     .option('-n, --number <numbers...>', 'specify numbers')
+//     .option('-l, --letter [letters...]', 'specify letters');
+
+
 program
-    .option('-n, --number <numbers...>', 'specify numbers')
-    .option('-l, --letter [letters...]', 'specify letters');
+    .on('option:debug', function() {
+        console.log('debug option event was emitted', this.opts());
+    })
+
+program
+    .on('option:small', function() {
+        console.log('small option event was emitted', this.opts());
+    })
+
+
+program
+    .on('command:set', function() {
+        console.log('command set event was emitted', this.args);
+    })
+
+// console.log(program.opts())
 
 program.parse(process.argv);
 
-console.log('Options: ', program.opts());
+// console.log('Options: ', program.opts());
 
 // 除去选项参数后，剩下的参数，可以认为是命令参数
-console.log('Remaining arguments: ', program.args);
+// console.log('Remaining arguments: ', program.args);
 
 
 // const options = program.opts();
